e635f23a1ddb1b59dc2f279791072c3c5f91aa6a
Mohammed Adenwala <adenwala@outlook.com>
2019-06-03 11:15:11 -0500

    Added ability to get devices

---
+++ src/Wink.Api.Uwp/Credentials.cs
@@ -0,0 +1,6 @@
+﻿internal static class Credentials
+{
+    public const string CLIENT_ID = "Rpe8nPvtp75XiLdrplcLpH0SyDrgfbvk";
+    public const string CLIENT_SECRET = "Y0JQVE6ayj8sXXRuc7sCf1dbbEKn6i-t";
+    public const string REDIRECT_URL = "http://www.adenwala.com/wink_redirect";
+}
